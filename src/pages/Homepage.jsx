a35109879421e55f50d901227d619f467848758f
madu5 <MM218@outlook.com.br>
2024-09-07 20:46:48 -0300

    styled homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,34 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Box } from '@mui/material';
+import logoLg from '../assets/logo-lg.png';
 
 import authControllers from '../controllers/authController';
 
 export default function Homepage() {
 
     return (
-        <>
-            <Typography variant='h3'>Spotify App</Typography>
-            <Button onClick={authControllers.handleAuthorizationRequest} >Log In</Button>
-        </>
+        <Box sx={
+            {
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '1rem',
+                height: '100vh',
+                backgroundImage: 'linear-gradient(to bottom, #383838, #181818 80%)',
+            }
+        }>
+            <img src={logoLg} alt="" />
+
+            <Button onClick={authControllers.handleAuthorizationRequest} 
+                variant="contained" size='large'
+                sx={{ 
+                    textTransform: 'none', bgcolor: '#1FDF64', color: 'black', fontWeight: 'bold',
+                    padding: '11px 125px', borderRadius: '50px', fontSize: 15, marginTop: 9,
+                    '&:hover': { transform: 'scale(1.03)', transition: '0.1s' }
+                }}
+            >
+                Log In
+            </Button>
+        </Box>
     );
 };
